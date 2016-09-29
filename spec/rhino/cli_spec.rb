f0c6e0fc9126f2969c5ed6ea032f2d8a8d3f8b1f
Kevin Sylvestre <kevin@ksylvest.com>
2016-09-29 02:07:04 -0700

    avoiding signal trap for EXIT given new return flow

--- spec/rhino/cli_spec.rb
+++ spec/rhino/cli_spec.rb
@@ -8,27 +8,23 @@ describe Rhino::CLI do
   describe "#parse" do
     %w(-v --version).each do |option|
       it "supports '#{option}' option" do
-        Signal.trap("EXIT") do
-          expect(Rhino.logger).to receive(:log).with Rhino::VERSION
-          cli.parse([option])
-        end
+        expect(Rhino.logger).to receive(:log).with Rhino::VERSION
+        cli.parse([option])
       end
     end
 
     %w(-h --help).each do |option|
       it "supports '#{option}'" do
-        Signal.trap("EXIT") do
-          expect(Rhino.logger).to receive(:log).with <<~DEBUG
-          usage: rhino [options] [./config.ru]
-              -h, --help     help
-              -v, --version  version
-              -b, --bind     bind (default: 0.0.0.0)
-              -p, --port     port (default: 5000)
-              --backlog      backlog (default: 64)
-              --reuseaddr    reuseaddr (default: true)
-          DEBUG
-          cli.parse([option])
-        end
+        expect(Rhino.logger).to receive(:log).with <<~DEBUG
+        usage: rhino [options] [./config.ru]
+            -h, --help     help
+            -v, --version  version
+            -b, --bind     bind (default: 0.0.0.0)
+            -p, --port     port (default: 5000)
+            --backlog      backlog (default: 64)
+            --reuseaddr    reuseaddr (default: true)
+        DEBUG
+        cli.parse([option])
       end
     end
 
